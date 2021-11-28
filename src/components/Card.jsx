046b6485429b96b95995ed8ae0731041dee2326e
ViniciusLussaniToigo <viniciuslussani@gmail.com>
2021-11-27 23:24:54 -0300

    iniciando-projeto

---
+++ src/components/Card.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class App extends React.Component {
+
+render ()  {
+
+    return(
+        <div>
+            <div className="box-pagina-principal" onClick={reproduzMensagem}>
+              <img src="https://picsum.photos/400/400?a=2 " alt="" />
+              <h4>{produto2}</h4>
+              <h3>39,99</h3>
+            </div>
+
+
+        </div>
+    )
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+    export default App;
